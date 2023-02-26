8ad96cf06f6a37dbb2642233456c7c0b9d5a3eb9
Diego Sánchez Escribano <diego.sanchez.escribano@pm.me>
2023-02-26 17:43:13 +0100

    build: chain_validation implemented

--- routers/tasks.py
+++ routers/tasks.py
@@ -10,14 +10,21 @@ router = APIRouter(prefix="/tasks",
                    responses={status.HTTP_404_NOT_FOUND: {"error": "Not found"}})
 
 
+# -------------------------------------------- CRUD ---------------------------------------------------------------#
 @router.get("/", response_model=list[Task])
 async def tasks():
-    task_list = tasks_schema(db_client.tasks.find())
+    task_list = get_tasks_list()
 
     if not task_list:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No tasks have been created")
     else:
-        return task_list
+        valid_tasks_chain = chain_validation(task_list)
+        if valid_tasks_chain:
+            return task_list
+        else:
+            raise HTTPException(status_code=status.HTTP_417_EXPECTATION_FAILED,
+                                detail={"error": f"valid_chain={valid_tasks_chain}"}
+                                )
 
 
 @router.post("/task", response_model=Task, status_code=status.HTTP_201_CREATED)
@@ -26,11 +33,22 @@ async def task(task: Task):
 
     db_client.tasks.insert_one(task_dict)
 
-    new_task = task_schema(db_client.tasks.find_one({"id_hash": task_dict["id_hash"]}))
+    tasks_list = get_tasks_list()
 
-    return Task(**new_task)
+    valid_tasks_chain = chain_validation(tasks_list)
+
+    if valid_tasks_chain:
+
+        new_task = task_schema(db_client.tasks.find_one({"id_hash": task_dict["id_hash"]}))
+        return Task(**new_task)
+
+    else:
+        raise HTTPException(status_code=status.HTTP_417_EXPECTATION_FAILED,
+                            detail={"error": f"valid_chain={valid_tasks_chain}"}
+                            )
 
 
+# ----------------------------------------- Functions ------------------------------------------------------------#
 def create_new_task(task: Task):
     task.timestamp = datetime.now()
     task.last_hash = search_last_hash()
@@ -40,8 +58,13 @@ def create_new_task(task: Task):
     return dict(task)
 
 
-def search_last_hash():
+def get_tasks_list() -> list[task_schema(Task)]:
     task_list = tasks_schema(db_client.tasks.find())
+    return task_list
+
+
+def search_last_hash():
+    task_list = get_tasks_list()
 
     if not task_list:
         return ""
@@ -56,3 +79,14 @@ def create_hash(task: Task):
 
     hash = sha256(data_string.encode()).hexdigest()
     return str(hash)
+
+
+def chain_validation(tasks: list[task_schema(Task)]) -> bool:
+    valid_chain = True
+
+    for task in range(1, len(tasks)):
+        if tasks[task]["last_hash"] != tasks[task - 1]["id_hash"]:
+            valid_chain = False
+            return valid_chain
+
+    return valid_chain
